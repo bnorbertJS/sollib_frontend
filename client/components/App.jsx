b1c0fa9c3bdb9e1be2dc7f76e803979200edf6fe
Norbert Braun <bnorbertjs@gmail.com>
2018-02-27 15:43:47 +0100

    init

---
+++ client/components/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default class App extends React.Component {
+  render() {
+    return (
+     <div style={{textAlign: 'center'}}>
+        <h1>Solution Library</h1>
+      </div>);
+  }
+}
---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+
+module.exports = {
+  entry: './client/index.js',
+  output: {
+    path: path.resolve('public'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
+      { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ }
+    ]
+  },
+  plugins: [new HtmlWebpackPlugin({template: './index.html'})]
+};
